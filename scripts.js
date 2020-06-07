1864e81dd4da6272e9022373a53399d5b7f0546e
Daniel Damak <damak.daniel@gmail.com>
2020-06-07 18:58:17 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+   console.log("My name is Daniel"); 
+   let colors = ["orange","blue","green","purple"];
+   let post = {
+     title: "My Post",
+     description: "My first post, awesome",
+     likeCount: 12,
+     comments: ["Cool!","Nice job!"]
+    };
+
+
+    console.log(colors);
+    console.log(post);
